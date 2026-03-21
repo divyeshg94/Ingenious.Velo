098b708b1c4583a04fa22175e21708831fe07dbf
Divyesh Govaerdhanan <divyesh.govaerdhanan@plateauinc.com>
2026-03-21 08:40:54 -0400

    Angular build fix

--- src/Velo.Extension/src/app/shared/services/index.ts
+++
@@ -1,5 +0,0 @@
-// Export all services from this barrel
-export * from './dora-metrics.service';
-export * from './org-connection.service';
-export * from './sdk-initializer.service';
-export * from './mock-sdk.service';
